026df4e58e54ddd860f779459bf741341301c22e
avinashpipliyacode <avinashpipliya@gmail.com>
2017-09-07 01:28:15 +0530

    First Commit

---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyA6KOvlkOSXI0iEqSmtS3eJwUmrz82SOZE",
+    authDomain: "react-notes-f70e6.firebaseapp.com",
+    databaseURL: "https://react-notes-f70e6.firebaseio.com",
+    projectId: "react-notes-f70e6",
+    storageBucket: "react-notes-f70e6.appspot.com",
+    messagingSenderId: "153939812881"
+  };
